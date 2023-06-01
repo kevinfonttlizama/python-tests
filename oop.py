f6dd8aae3bac11315ae715eb814b2d71e807a4b6
kevin <kevin,andresfontt@gmail.com>
2023-06-01 04:47:56 -0400

    funcion y metodo

--- oop.py
+++ oop.py
@@ -7,6 +7,27 @@
 
 #la programacion orientada a objetos como dice su nombre es un paradigma de la programacion que se creo para reemplazar o mejorar la forma de programar a lo que era el paradigma de la programacion orientada a procesos en lenguajes antiguos como cobol o visual basic por ej.
 
+#la programamcion orientada a objetos tiene como finalidad simular objetos de la vida real para hacer mas simple a los programadores construir programas/software basado en los comportamientos de estos objetos usando sus caracteristicas, comportamiento,aspecto etc...
+
+class Car():
+    chasisHeight=250
+    chasisWidth=120
+    wheels=4
+    moving=False
+
+#----- Funcion vs metodo --------------------------
+
+#diferencia entre funcion y metodo 
+#Una Funcion no es exclusiva de una clase
+#un Metodo es una funcion especial que pertenece a una clase 
+
+
+
+
+
+
+    
+    
 
 
 
