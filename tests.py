6fcbaa70f97c9cbeb9c2b1264d23bdf4e9087124
kevin <kevin,andresfontt@gmail.com>
2023-06-01 03:17:51 -0400

    ejemplo de try y finally

---
+++ tests.py
@@ -0,0 +1,25 @@
+#exceptions parte 2 ya que no queria hacer un documento mas largo solo eso, por eso este otro archivo
+
+#function divide con dos Exceptions 
+
+# def divide():
+#     try:
+#         option1=(float(input("Enter the first number:   ")))
+#         option2=(float(input("Enter the second number:   ")))
+    
+#         print("the division is: " + str(option1/option2))
+
+#     except ValueError:
+#         print("incorrect values")
+
+#     except ZeroDivisionError:
+#         print("cannot divide zero!")
+
+#     finally:
+#         print("finished")
+
+# divide()
+
+#finally siempre se ejecuta aunque no tengamos definidas excepciones pero si hay un try sin except nos dara un error de sintaxis.
+#finally es un bloque de codigo que se ejecuta despues de las excepciones, siempre se ejecuta si o si.
+
