32c2bc613089c43f9b7c8ea94351639962ecaeba
kevin <kevin,andresfontt@gmail.com>
2023-06-01 04:37:52 -0400

    definicion basica de poo

---
+++ oop.py
@@ -0,0 +1,12 @@
+#---------Programacion orientada a objetos POO------------
+#Object Oriented Programming OOP
+
+
+#nomenclatura del punto
+#esta nomenclatura sirve para acceder a las caracteristicas, atributos, valores de los datos para modificarlos, añadirlos, actualizarlos etc..
+
+#la programacion orientada a objetos como dice su nombre es un paradigma de la programacion que se creo para reemplazar o mejorar la forma de programar a lo que era el paradigma de la programacion orientada a procesos en lenguajes antiguos como cobol o visual basic por ej.
+
+
+
+
