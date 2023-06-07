b0f268513dc586deb57a2ab26f5348a9b1f6b55b
kevin <kevin,andresfontt@gmail.com>
2023-06-06 21:21:52 -0400

    ejemplos de manipulacion de cadenas

---
+++ string-manipulation.py
@@ -0,0 +1,35 @@
+#------------metodos de manipulacion de cadenas en python ---------
+
+exampleString = "hola a todos"
+exampleString2 = "HOLA A TODOS"
+exampleString3 = "Hola a todos"
+
+print(exampleString.upper()) #se pasa todo el texto a mayuscula
+
+print(exampleString2.lower()) #convierte toda la cadena a minusculas
+
+print(exampleString.capitalize()) #Coloca una mayuscula en la primera palabra
+
+print(exampleString.count()) #contar cuantas veces aparece una letra o una cadena dentro de un texto o una frase
+
+print(exampleString.find()) #representar el indice o posicion donde se encuentra un caracter o un grupo de caracteres dentro de un texto
+
+print(exampleString.isdigit()) #devulve true o false si el valor/atributo es un numero
+
+print(exampleString.isalnum())#comprobar si son caracteres alphanumericos
+
+print(exampleString.isalpha())#comprueba si son solo letras los espacios no cuentan 
+
+print(exampleString.split())#separa por palabras usando espacios
+
+print(exampleString.strip()) #borrar los espacios sobrantes al inicio y al final
+
+print(exampleString.replace())#cambia una palabra o letra por otra 
+
+print(exampleString.find())#representa el indice de un caracter, pero contando desde atras, al contrario de find
+
+#no son todos pero es para refrescarme la memoria jaja y hay algunos metodos que necesitan argumentos asi que aun no esta finalizado este archivo de ejemplo.
+
+
+
+ 
